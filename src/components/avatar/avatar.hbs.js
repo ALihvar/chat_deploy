4dac0c86f0de65c4016759840412d566dc977ef5
A <spacetps@ya.ru>
2022-11-10 00:18:25 +0300

    new version

--- src/components/avatar/avatar.hbs.js
+++
@@ -1,3 +0,0 @@
-export default `
-  <div class='avatar'></div>
-`;
--- src/modules/sidebarContent/modules/field/types.ts
+++
@@ -1,8 +0,0 @@
-export interface FieldProps {
-  title: string;
-  name: string;
-  value: string;
-  events?: Record<string, (event: Event) => void>;
-  type?: string;
-  pattern: string;
-}
